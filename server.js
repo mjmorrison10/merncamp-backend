be6610f87f3e4016a27f11a22d7a3b968886f4a1
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2022-02-25 13:46:17 -0800

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://merncamp-node.herokuapp.com/"],
+    // origin: ["http://localhost:3000"],
   })
 );
 
